ec398486eae0fb66471f248053c6d1c8b2e2fa10
NickolasHKraus <NickHKraus@gmail.com>
2021-01-29 10:09:10 -0600

    Add update.sh

---
+++ update.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# Use `/usr/bin/env bash` in order to search $PATH for `bash`.
+
+# create directories
+mkdir -p boot/loader/
+mkdir -p boot/loader/entries/
+mkdir -p etc/
+mkdir -p etc/pacman.d/
+mkdir -p etc/systemd/network/
+
+# touch files
+touch boot/loader/entries/arch.conf
+touch boot/loader/loader.conf
+touch etc/fstab
+touch etc/hostname
+touch etc/hosts
+touch etc/locale.conf
+touch etc/locale.gen
+touch etc/pacman.d/mirrorlist
+touch etc/systemd/network/20-ethernet.network
+touch etc/systemd/network/20-wireless.network
+touch etc/vconsole.conf
+
+# copy files
+cp /boot/loader/entries/arch.conf boot/loader/entries/arch.conf
+cp /boot/loader/loader.conf boot/loader/loader.conf
+cp /etc/fstab etc/fstab
+cp /etc/hostname etc/hostname
+cp /etc/hosts etc/hosts
+cp /etc/locale.conf etc/locale.conf
+cp /etc/locale.gen etc/locale.gen
+cp /etc/pacman.d/mirrorlist etc/pacman.d/mirrorlist
+cp /etc/systemd/network/20-ethernet.network etc/systemd/network/20-ethernet.network
+cp /etc/systemd/network/20-wireless.network etc/systemd/network/20-wireless.network
+cp /etc/vconsole.conf etc/vconsole.conf
